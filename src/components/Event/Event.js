8966fa9e3f01381f31c4acec5941de38138f9d0f
Paul Tower <ptower@hypercubeapps.com>
2018-07-05 16:15:52 -0400

    Oops

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -20,21 +20,9 @@ export default class Event extends PureComponent {
     let time = event ? (current ? event.end : event.start) : 0
 
     return event && (
-<<<<<<< HEAD
-      <div className="Event">
-        <div className="item">
-          <div className="name">{event.summary + (event.private ? ' 👀' : '')}</div>
-          <div className="badge">{current ? 'Ends' : 'Starts'} at {moment(time).format('h:mm a')}</div>
-        </div>
-        <div className="item">
-          <div className="name">Another Event that has a long long title</div>
-          <div className="badge">Starts at 5:30 pm</div>
-        </div>
-=======
       <div className="item">
         <div className="name">{event.summary + (event.private ? ' 👀' : '')}</div>
         <div className="badge">{current ? 'Ends' : 'Starts'} at {moment(time).format('h:mm a')}</div>
->>>>>>> New components
       </div>
     )
   }
